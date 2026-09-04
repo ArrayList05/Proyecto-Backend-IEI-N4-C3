d7cc37befc0cc988d62ffaebf09f30a011021771
ArrayList <ximena.ortiz09@inacapmail.cl>
2026-09-04 17:37:20 -0400

    Bienvenida corregida

--- drf/settings.py
+++ drf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rse-91g&c45)54g+0-q3^_$9@#^dg-g30@*fyur_xo&wk7cg!m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'localhost:8000']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
 
 
 # Application definition

7e23a2419e39d2ef3ef2f1d4757ca4d435ee6524
ArrayList <ximena.ortiz09@inacapmail.cl>
2026-09-04 01:18:58 -0400

    Finalizacion del proyecto: bienvenida y 404

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
+# Proyecto: Sistema de Gestión de Veterinaria
+Proyecto de backend para la centralizacion y seguimiento de historial
+de pacientes, dueños y atenciones medicas veterinarias.
+
+## Requisitos e instalación
+1. clonar el repositorio
+2. crear y activar el ambiente virtual:
+```
+python -m venv ambiente1.\ambiente1\Scripts\activate
+```
+3. instalar dependencias
+```
+pip install -r requirements.txt
+´´´
+4. iniciar el servidor
+```
+python manage.py runserver
+´´´
+
+
+
 # IEI_N4_C3
 Clases de Backend con Django
 
--- drf/settings.py
+++ drf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rse-91g&c45)54g+0-q3^_$9@#^dg-g30@*fyur_xo&wk7cg!m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'localhost:8000']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'drf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
